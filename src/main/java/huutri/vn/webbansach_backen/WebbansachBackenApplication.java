06db10047b44fa58abaeb38485ebf253afb2ddce
Duong Huu Tri <huutri12022004@gmail.com>
2026-01-12 20:45:16 +0700

    new project

---
+++ src/main/java/huutri/vn/webbansach_backen/WebbansachBackenApplication.java
@@ -0,0 +1,13 @@
+package huutri.vn.webbansach_backen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbansachBackenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebbansachBackenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/huutri/vn/webbansach_backen/WebbansachBackenApplicationTests.java
@@ -0,0 +1,13 @@
+package huutri.vn.webbansach_backen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbansachBackenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
